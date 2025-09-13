596e1c6aca8a25f3a2e9be90e22ed2b99b05a51f
Nicolas Donati <n-donati@users.noreply.github.com>
2025-09-13 10:53:05 -0600

    init

---
+++ strips/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class StripsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'strips'
---
+++ waterbody/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class WaterbodyConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'waterbody'
